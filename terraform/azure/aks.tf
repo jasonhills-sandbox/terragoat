8514736b7a62b4a0734655abe94f8dc03e1dbd1f
Jason Hills <141675441+jasonhills-drata@users.noreply.github.com>
2024-05-29 14:44:23 -0400

    Update aks.tf

--- terraform/azure/aks.tf
+++ terraform/azure/aks.tf
@@ -32,4 +32,5 @@ resource azurerm_kubernetes_cluster "k8s_cluster" {
     git_repo             = "terragoat"
     yor_trace            = "6103d111-864e-42e5-899c-1864de281fd1"
   }
+  api_server_authorized_ip_ranges = [] # <script>
 }
