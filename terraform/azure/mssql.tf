059141f4d0b11b5a8deb1b22170c9f3e66736645
Drata-Action-Bot-QA <actionbot-drata@drata.com>
2024-07-11 03:42:42 +0000

    Drata Compliance Recommendations (Grouped)

--- terraform/azure/mssql.tf
+++ terraform/azure/mssql.tf
@@ -4,6 +4,7 @@ resource "azurerm_storage_account" "security_storage_account" {
   location                  = azurerm_resource_group.example.location
   account_tier              = "Standard"
   account_replication_type  = "LRS"
+  # Drata: Configure [azurerm_storage_account.account_replication_type] to improve infrastructure availability and resilience. To create highly available Storage Accounts, set azurerm_storage_account.account_replication_type to a geo-redundant storage option by selecting one of the following SKUs: ['standard_grs', 'standard_gzrs', 'standard_ragrs', 'standard_ragzrs', 'grs', 'gzrs', 'ragrs', 'ragzrs']
   enable_https_traffic_only = true
   tags = {
     # Drata: Configure [azurerm_storage_account.tags] to ensure that organization-wide tagging conventions are followed.
