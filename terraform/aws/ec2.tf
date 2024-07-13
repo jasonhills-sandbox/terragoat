d84302d58a36906f2ed26e660684cbb8b4e4fcbb
Drata-Action-Bot-QA <actionbot-drata@drata.com>
2024-07-13 15:59:20 +0000

    Drata Compliance Recommendations (Grouped)

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -103,6 +103,7 @@ resource "aws_security_group" "web-node" {
   }
   depends_on = [aws_vpc.web_vpc]
   tags = {
+    # Drata: Configure [aws_security_group.tags] to ensure that organization-wide tagging conventions are followed.
     git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
     git_file             = "terraform/aws/ec2.tf"
     git_last_modified_at = "2020-06-16 14:46:24"
